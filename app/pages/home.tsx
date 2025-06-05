d17390361d44f9db9c236e519a2e08b4de8d0de5
Simón Espinosa <saespinosac@gmail.com>
2025-06-04 20:20:55 -0400

    chore: add comments

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -44,19 +44,23 @@ export default function Home() {
         }
     }, [debouncedSearchText, pokemonList]);
 
+    // We use useMemo so that the total number of pages doesn’t have to be recalculated at every moment. Also, that total number of Pokémon is divided by 20, which is the amount that will be shown on each page.
     const totalPages = useMemo(() => Math.ceil((pokemonsResponse?.count ?? 0) / 20), [pokemonsResponse?.count]);
 
 
     return (
+
         !loading && !loadingListSearched ? (
                 <main className={styles.cMain}>
                     <SearchInput value={searchText} onChange={handleSearch}/>
                     <section className={styles.cGrid}>
                         {
+                            /*There are two different render ways to handle live search and pagination*/
                              pokemonsSearched.length > 0 ? pokemonsSearched.map((pokemon: Pokemon) => <Card key={pokemon.id} pokemon={pokemon}/>) :
                              pokemonsResponse?.results.map((pokemon: Pokemon) => <Card key={pokemon.id} pokemon={pokemon}/>)
                         }
                     </section>
+                    {/* Pagination component will not render at live search */}
                     { pokemonsSearched.length === 0 && <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}/> }
                 </main>
             )
